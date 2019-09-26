73af6370bb24248bbcf228406957a8520a001427
unknown <majid13751103@gmail.com>
2019-09-26 09:31:34 +0330

    create about page

---
+++ weblog/resources/views/about.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>About</title>
+</head>
+<body>
+<h1 style="color:green;text-align:center;font-weight:italic">About Page</h1>
+</body>
+</html>
