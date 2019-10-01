d254a8d48fc54708e91fbc0b608ce089d7a41b51
alux <ahmadabd13741112@gmail.com>
2019-10-01 10:53:06 +0330

    put navbar in nav.blade.php

--- weblog/resources/views/layout.blade.php
+++ weblog/resources/views/layout.blade.php
@@ -16,44 +16,11 @@
 		<script src="js/jquery.min.js"></script>
 
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script> 
+		
 		<title>@yield('title')</title>
 	</head>
 	<body>
-		<nav class="navbar navbar-expand-md bg-info navbar-dark" style="flex-direction: row-reverse;" id="nav1"> 
-				
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="collapsibleNavbar" style="justify-content: flex-end;">
-			
-				<ul class="navbar-nav" style="flex-direction:row-reverse;height:50px;padding:5px">
-					<li class="nav-item">
-						<a class="navbar-brand" href="#" style="margin-right:15px;">صفحه اول</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#" style="margin-right:15px;">تماس و درباره ما</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#" style="margin-right:15px;">پروژه ها</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#" style="margin-right:15px;">ویدیوها</a>
-					</li>
-					<li class="nav-item">
-							<a class="nav-link" href="#" style="margin-right:15px">پادکست ها</a>
-					</li>    
-				</ul>
-			</div>  
-	
-			<div class="collapse navbar-collapse" id="collapsibleNavbar">
-				<form class="form-inline mr-auto">
-					<input class="form-control text-right" type="text" placeholder="جستجو" aria-label="Search">
-					<button class="btn btn-info-color btn-rounded btn-md my-0 ml-sm-2 btn-primary" type="submit">جستجو</button>
-				</form>
-			</div>
-		</nav>
+		@include('nav')
 
 		<!-- content -->
 		<div class="container-fluid" style="margin-top:15px">
---
+++ weblog/resources/views/nav.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-md bg-info navbar-dark" style="flex-direction: row-reverse;" id="nav1"> 
+				
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar" style="justify-content: flex-end;">
+    
+        <ul class="navbar-nav" style="flex-direction:row-reverse;height:50px;padding:5px">
+            <li class="nav-item">
+                <a class="navbar-brand" href="#" style="margin-right:15px;">صفحه اول</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" style="margin-right:15px;">تماس و درباره ما</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" style="margin-right:15px;">پروژه ها</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" style="margin-right:15px;">ویدیوها</a>
+            </li>
+            <li class="nav-item">
+                    <a class="nav-link" href="#" style="margin-right:15px">پادکست ها</a>
+            </li>    
+        </ul>
+    </div>  
+
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <form class="form-inline mr-auto">
+            <input class="form-control text-right" type="text" placeholder="جستجو" aria-label="Search">
+            <button class="btn btn-info-color btn-rounded btn-md my-0 ml-sm-2 btn-primary" type="submit">جستجو</button>
+        </form>
+    </div>
+</nav>
